db1276bd8aa50e07aea6aa06b816c5a2c88dee47
Anthony M Hernandez <55599397+asvka@users.noreply.github.com>
2020-01-26 16:24:35 -0800

    Made axios call, and usestate hook in characterlist. Imported and configured Route on App.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import CharacterCard from './CharacterCard';
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [char, setChar] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+    .get('https://rickandmortyapi.com/api/character/')
+    .then(res => {
+      setChar(res.data.results);
+      console.log(res.data.results);
+      console.log(res);
+    })
+    .catch(err => {
+      console.error('Error in getting schwifty!', err)
+    });
   }, []);
+  console.log(char);
+
 
   return (
     <section className="character-list">
